3b030ed14294082d8700ec68e3eb34e62a2a9ed1
Arthur <arthurleray2@gmail.com>
2019-05-21 15:40:22 +0200

    Version before deleting CarbonIntensity from db

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const express = require('express');
 const moment = require('moment');
 const bodyParser = require('body-parser');
 const objection = require('objection');
+const axios = require('axios');
 
 const Person = require('./src/database/models/Person');
 const Car = require('./src/database/models/Car');
@@ -18,31 +19,89 @@ app.use(bodyParser.urlencoded({
 }));
 
 
+app.get('/rentals', (req, res) =>
+{
+    Rental.query().then(rentalList => res.send(rentalList));
+
+});
+
 app.post('/addRental', async (req, res) =>
 {
-    await Rental.query().insert(
+    //Check for person existence
+    if(await isPersonInTable(req.body.person_id))
+    {
+        Person.query().findById(req.body.person_id).then(async person =>
         {
-            from: moment(req.body.from).toISOString(),
-            to: moment(req.body.to).toISOString(),
-            car_id: req.body.car_id,
-            person_id: req.body.person_id,
+            //Check for car existence
+            if(await isCarInTable(req.body.car_id))
+            {
+               Car.query().findById(req.body.car_id).then(async car =>
+               {
+                   //Get carbon intensity for specified date
+                   await getIntensityFromDate(req.body.from);
+
+
+               });
+            }
+
         });
-    res.end();
+    }
+    res.send('waiting');
 });
+
 
 
 
 app.listen(3000, () => console.log('Server listening'));
 
 
-
-
-
-function getPersons()
+function isPersonInTable(id)
 {
-    return Person.query().findById(req.body.person_id)
+    return Person.query().where('id', id).then(tableData =>
+    {
+        return tableData.length > 0;
+    });
 }
 
+function isCarInTable(id)
+{
 
+    return Car.query().where('id', id).then(tableData =>
+    {
+        return tableData.length > 0;
+    });
 
+}
 
+function getIntensityFromDate(date)
+{
+    return axios.get('https://api.carbonintensity.org.uk/intensity/date/' + moment(date).format('YYYY-MM-DD')).then(async intensityList =>
+    {
+        for(const anIntensity of intensityList.data.data)
+        {
+            if(await isDateInTable(date))
+            {
+                break;
+            }
+            else
+            {
+                await CarbonIntensity.query().insert({
+                    from: anIntensity.from,
+                    to: anIntensity.to,
+                    forecast: anIntensity.intensity.forecast,
+                    actual: anIntensity.intensity.actual,
+                    index: anIntensity.intensity.index,
+                });
+            }
+
+        }
+    });
+}
+
+function isDateInTable(date)
+{
+    return CarbonIntensity.query().where('from', date).then(tableData =>
+    {
+        return tableData.length > 0;
+    });
+}
